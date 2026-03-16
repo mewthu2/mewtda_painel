b6d025e6e2cbf2813affa7ad6e1c1d60d61f083b
danielkbr <danielkbr89@gmail.com>
2026-03-16 17:17:06 -0300

    fix

--- app/controllers/integrations/shopify_events_controller.rb
+++ app/controllers/integrations/shopify_events_controller.rb
@@ -1,13 +1,19 @@
 class Integrations::ShopifyEventsController < IntegrationController
   def create
     ShopifyEvent.create!(
+      client: current_integration.client,
       integration_user: current_integration,
+
+      kind: params[:name],
       event_id: params[:id],
       event_name: params[:name],
       event_type: params[:type],
+
       session_id: params[:session_id] || params[:clientId],
+
       shop_domain: params.dig(:context, :shop_domain),
       event_timestamp: params[:timestamp],
+
       data: params[:data],
       context: params[:context],
       raw_payload: params.to_unsafe_h
